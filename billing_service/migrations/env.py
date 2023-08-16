6f5ac460e1a8f8aa5ef321a3ac24403183efe591
mod-web <module-web@yandex.ru>
2023-08-16 16:41:27 +0300

    env

--- billing_service/migrations/env.py
+++ billing_service/migrations/env.py
@@ -4,9 +4,6 @@ from sqlalchemy import pool
 from alembic import context
 
 from config import settings
-
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 from src.models.models import metadata
 
 config = context.config
@@ -17,6 +14,9 @@ config.set_section_option(section, 'DB_PORT', settings.DB_PORT)
 config.set_section_option(section, 'DB_USER', settings.DB_USER)
 config.set_section_option(section, 'DB_NAME', settings.DB_NAME)
 config.set_section_option(section, 'DB_PASS', settings.DB_PASS)
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
